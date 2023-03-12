c29181de0657fc199fdcb81ad3c734c6fb88df3a
Erik Benito <erikbenito_2017@outlook.com>
2023-03-12 14:21:02 -0300

    first commit

---
+++ sistemaAutonomoBCCIII/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sistemaAutonomoBCCIII
+{
+    public partial class ContainerInicial : Form
+    {
+        public ContainerInicial()
+        {
+            InitializeComponent();
+        }
+    }
+}
